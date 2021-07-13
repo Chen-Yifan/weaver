d8ec5c2bb918bea750a053777ae986fa645b4b3b
fffibonacci <yifanchen@intl.zjuu.edu.ch>
2021-07-13 01:59:18 -0500

    update multigpus

---
+++ download.sh
@@ -0,0 +1,4 @@
+for N in {21..90}
+do
+   wget http://opendata.cern.ch/record/12102/files/assets/cms/datascience/HiggsToBBNtupleProducerTool/HiggsToBBNTuple_HiggsToBB_QCD_RunII_13TeV_MC/train/ntuple_merged_$N.root
+done
--- root_train.sh
+++ root_train.sh
@@ -2,9 +2,9 @@
  --data-train 'dataset/train_root/ntuple_merged_*.root' \
  --data-config data/ak15_points_pf_sv.yaml \
  --network-config networks/in_pf_sv.py  \
- --model-prefix model_checkpoints/ \
- --num-workers 3 \
- --gpus 0 \
+ --model-prefix model_checkpoints/root10-29 \
+ --num-workers 5 \
+ --gpus 0,1,2,3 \
  --batch-size 512 \
  --start-lr 5e-3 \
  --num-epochs 20 \
